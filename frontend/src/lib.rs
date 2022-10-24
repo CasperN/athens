981284bc0fe65abfd44c7b50b226874caa2195df
Casper Neo <cneo@google.com>
2022-10-23 21:49:39 -0400

    Add some styling
    Go for a cool retro purple terminal vibe.

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -237,30 +237,11 @@ impl Component for List {
 
 #[function_component(App)]
 fn app() -> Html {
-    log::info!("app running");
-
-    let color = use_state(|| "red");
-    let cb = {
-        let color = color.clone();
-        Callback::from(move |_| {
-            log::info!("changing color!");
-            let next_color = match *color {
-                "red" => "green",
-                "green" => "blue",
-                "blue" => "cyan",
-                "cyan" => "magenta",
-                _ => "red",
-            };
-            color.set(next_color)
-        })
-    };
-
-    let style = format!("background-color:{};", *color);
     html! {
         <>
-        <h1 style={style}>
-            { "Hello world!" }
-        <button onclick={ cb.clone() }>{ "Color me!" }</button>
+        <link href="public/style.css" rel="stylesheet"/>
+        <h1>
+        { "Lists!" }
         </h1>
         <List/>
         </>
